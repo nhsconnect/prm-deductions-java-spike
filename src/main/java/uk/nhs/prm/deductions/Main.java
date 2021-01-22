5e29a9172b564896f5b34c1b3e0ffe4dabb65b6a
ewrobel-nhs <edyta.wrobel@nhs.net>
2021-01-22 15:49:01 +0000

    [PRMT-1492] Tried amqp with qpid

---
+++ src/main/java/uk/nhs/prm/deductions/Main.java
@@ -0,0 +1,10 @@
+package uk.nhs.prm.deductions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java");
+        Consumer consumer = new Consumer();
+        consumer.run();
+    }
+}
