51259091bb3e828bd5fc7bedf37c3844ba478e74
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-01-25 16:54:11 +0000

    [PRMT-1492] Sent message to unhandled queue

--- src/main/java/uk/nhs/prm/deductions/test/ActiveMQConfig.java
+++ src/main/java/uk/nhs/prm/deductions/test/ActiveMQConfig.java
@@ -1,12 +1,14 @@
 package uk.nhs.prm.deductions.test;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 
--- src/main/java/uk/nhs/prm/deductions/test/JmsConsumer.java
+++ src/main/java/uk/nhs/prm/deductions/test/JmsConsumer.java
@@ -1,7 +1,9 @@
 package uk.nhs.prm.deductions.test;
 
 import org.apache.activemq.command.ActiveMQTextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Message;
@@ -9,6 +11,9 @@ import javax.jms.MessageListener;
 
 @Component
 public class JmsConsumer {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
 
     public JmsConsumer() {
         System.out.println("in jms consumer");
@@ -19,6 +24,7 @@ public class JmsConsumer {
         try {
             ActiveMQTextMessage objectMessage = (ActiveMQTextMessage)message;
             System.out.println("Received Message: "+ objectMessage.getText());
+            jmsTemplate.convertAndSend("unhandled-inbound", objectMessage);
 
         } catch(Exception e) {
             System.out.println("Received Exception : "+ e);
