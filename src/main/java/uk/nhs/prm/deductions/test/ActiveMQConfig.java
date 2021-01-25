c4dbe2b6835eb23cb93a67a6bbaa38837bc11f38
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-01-25 13:52:27 +0000

    [PRMT-1492] Implementing failover

--- src/main/java/uk/nhs/prm/deductions/test/ActiveMQConfig.java
+++ src/main/java/uk/nhs/prm/deductions/test/ActiveMQConfig.java
@@ -10,6 +10,8 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 
 import javax.jms.ConnectionFactory;
 
+import static javax.jms.Session.CLIENT_ACKNOWLEDGE;
+
 @Configuration
 public class ActiveMQConfig {
 
@@ -20,6 +22,7 @@ public class ActiveMQConfig {
     public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setSessionAcknowledgeMode(CLIENT_ACKNOWLEDGE);
         // This provides all boot's default to this factory, including the message converter
         configurer.configure(factory, connectionFactory);
         // You could still override some of Boot's default if necessary.
--- src/main/java/uk/nhs/prm/deductions/test/JmsConsumer.java
+++ src/main/java/uk/nhs/prm/deductions/test/JmsConsumer.java
@@ -8,18 +8,18 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 
 @Component
-public class JmsConsumer implements MessageListener {
+public class JmsConsumer {
 
     public JmsConsumer() {
         System.out.println("in jms consumer");
     }
 
-    @Override
     @JmsListener(destination = "${active-mq.queue}")
-    public void onMessage(Message message) {
+    public void onMessage(Message message) throws Exception {
         try {
             ActiveMQTextMessage objectMessage = (ActiveMQTextMessage)message;
             System.out.println("Received Message: "+ objectMessage.getText());
+
         } catch(Exception e) {
             System.out.println("Received Exception : "+ e);
         }
